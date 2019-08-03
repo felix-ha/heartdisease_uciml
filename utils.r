4135334bab98ac45b1a704258c665e01fc0f297c
Felix Hauer <hauerf98@gmail.com>
2019-08-03 14:24:07 +0200

    mend

--- utils.r
+++ utils.r
@@ -104,6 +104,31 @@ get_varibale_description <- function(variable) {
   
 }
 
+visualize_metrics <- function(df, fit, threshold) {
+  y_probabilities <- unname(predict(fit, df,  type="response"))
+  y_predicted <- ifelse(y_probabilities > threshold, 1, 0)
+  y_true <- ifelse(df$target == "no_disease", 0, 1)
+  
+  accuracy <- Accuracy(y_true = y_true, y_pred = y_predicted)
+  precision <- Precision(y_true = y_true, y_pred = y_predicted, positive = "1")
+  recall <- Recall(y_true = y_true, y_pred = y_predicted, positive = "1")
+  f_1 <- F1_Score(y_true = y_true, y_pred = y_predicted, positive = "1")
+  confusion_matrix <- ConfusionMatrix(y_true = y_true, y_pred = y_predicted)
+  auc <- AUC(y_true = y_true, y_pred = y_probabilities)
+  
+  print(paste("Metrics for fit with threshold", threshold))
+  print("Confusion Matrix    : ")
+  print(confusion_matrix)
+  print(paste("Accuracy  : ", round(accuracy, 3)))
+  print(paste("Precision : ", round(precision, 3)))
+  print(paste("Recall    : ", round(recall, 3)))
+  print(paste("F1        : ", round(f_1, 3)))
+  print(paste("AUC       : ", round(auc, 3)))
+  
+  precrec_obj <- evalmod(scores = y_probabilities, labels = y_true)
+  autoplot(precrec_obj)
+}
+
 get_opt_df <- function() {
   df_raw <- read_csv("data.csv")
   heart <- get_df(df_raw)
