1b0f1ca17629fa7c4d28734476c1fbb45eadf0ef
Felix Hauer <hauerf98@gmail.com>
2019-08-03 14:23:39 +0200

    moved unused fuction, added function call to get training df

--- models.r
+++ models.r
@@ -11,38 +11,13 @@ library(recipes)
 library(class)
 
 
-visualize_metrics <- function(df, fit, threshold) {
-  y_probabilities <- unname(predict(fit, df,  type="response"))
-  y_predicted <- ifelse(y_probabilities > threshold, 1, 0)
-  y_true <- ifelse(df$target == "no_disease", 0, 1)
-  
-  accuracy <- Accuracy(y_true = y_true, y_pred = y_predicted)
-  precision <- Precision(y_true = y_true, y_pred = y_predicted, positive = "1")
-  recall <- Recall(y_true = y_true, y_pred = y_predicted, positive = "1")
-  f_1 <- F1_Score(y_true = y_true, y_pred = y_predicted, positive = "1")
-  confusion_matrix <- ConfusionMatrix(y_true = y_true, y_pred = y_predicted)
-  auc <- AUC(y_true = y_true, y_pred = y_probabilities)
-  
-  print(paste("Metrics for fit with threshold", threshold))
-  print("Confusion Matrix    : ")
-  print(confusion_matrix)
-  print(paste("Accuracy  : ", round(accuracy, 3)))
-  print(paste("Precision : ", round(precision, 3)))
-  print(paste("Recall    : ", round(recall, 3)))
-  print(paste("F1        : ", round(f_1, 3)))
-  print(paste("AUC       : ", round(auc, 3)))
-  
-  precrec_obj <- evalmod(scores = y_probabilities, labels = y_true)
-  autoplot(precrec_obj)
-}
-
-
-df_raw <- read_csv("data.csv")
-heart <- get_df(df_raw)
-heart <- get_training_df(heart)
+heart <- get_opt_df()
 heart_dummies <- get_df_dummies(heart)
 
 # do not use test set while model optimization!
+# df_raw <- read_csv("data.csv")
+# heart <- get_df(df_raw)
+# heart_trainig <- get_training_df(heart)
 # heart_test <- get_test_df(heart)
 
 set.seed(25)
