c28d39712230f5908b7608b45e9b62fbab6d9f3b
Felix Hauer <hauerf98@gmail.com>
2019-09-02 19:47:45 +0200

    new structure of repository

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # heartdisease_uciml
+
+This repository presents methods for classificaiton with the heart disease data set from the uci machine learning repository [uci machine learning repository](https://archive.ics.uci.edu/ml/datasets/Heart+Disease). Main focus will be logistic regression and decision trees. 
+
---
+++ helpers.r
@@ -0,0 +1,32 @@
+library(tidyverse)
+library(caret)
+
+# create clean data set: e. g. sex is a numeric vector, needs to be a factor
+get_df <- function(df){
+  result <- df %>%
+    transmute(age,
+              thalach,
+              trestbps,
+              oldpeak,
+              ca,
+              chol,
+              restecg = factor(df$restecg, levels = c(0,1,2), labels = c("normal", "ST-T_abnormalty", "hypertrophy")),
+              fbs = factor(df$fbs, levels = c(0,1), labels = c("no", "yes")),
+              sex = factor(df$sex, levels = c(0,1), labels = c("Female", "Male")),
+              exang = factor(df$exang, levels = c(0,1), labels = c("no", "yes")),
+              cp = factor(df$cp, levels = c(0,1,2,3), labels = c("typical_angina", "atypical_angina","non-anginal_pain", "asymptomatic")),
+              slope = factor(df$slope, levels = c(0,1,2), labels = c("upsloping", "flat", "downsloping")),              
+              target = factor(df$target, levels = c(0,1), labels = c("no_disease", "disease")),
+              thal = factor(df$thal,levels = c(0,1,2,3), labels = c("normal", "fixed_defect", "reversable_defect", "?"))
+    )
+}
+
+get_training_df <- function() {
+  set.seed(25)
+  df_raw <- read_csv("data.csv")
+  heart <- get_df(df_raw)
+  inTraining <- createDataPartition(df$target, p = .80, list = FALSE)
+  training <- df[ inTraining,]
+  # testing  <- df[-inTraining,]
+  return(training)
+}
