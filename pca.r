741f01d33371f569d7eaefe88cf160bce48caa88
Felix Hauer <hauerf98@gmail.com>
2019-09-24 20:47:47 +0200

    rewrote pca, used heart data

--- pca.r
+++ pca.r
@@ -1,26 +1,48 @@
 rm(list = ls())
 library(tidyverse)
+source("helpers.r")
 
-data(iris)
-head(iris, 3)
+df <- get_training_df()
 
-iris <- iris[sample(nrow(iris)),]
+df_continuous <- df %>% select(
+thalach,
+trestbps,
+oldpeak,
+chol, age)
 
-log.ir <- log(iris[, 1:4])
-ir.species <- iris[, 5]
+pca <- prcomp(df_continuous, center = TRUE, scale = TRUE)
 
-ir.pca <- prcomp(log.ir,
-                 center = TRUE,
-                 scale. = TRUE) 
+principal_components <- pca$x
 
-print(ir.pca)
+variance <- pca$sdev^2
+df_pca_var <- tibble(prop_variance = round(variance / sum(variance) * 100, 1),
+                     pc = c(1:ncol(df_continuous)))
 
-pred_matrix <- predict(ir.pca, 
-        newdata=log.ir)
+ggplot(df_pca_var, aes(y = prop_variance, x = pc)) +
+  geom_point(size = 5) +
+  geom_line()
 
-df <- tibble(pc1 = pred_matrix[,1],
-             pc2 = pred_matrix[,2],
-             target = iris[, 5])
+df_pca <- tibble(PC1 = principal_components[,1], 
+                 PC2 = principal_components[,2],
+                 target = df$target)
 
-ggplot(df, aes(x = pc1, y = pc2, color = target))+
-  geom_point()
+ggplot(df_pca, aes(x = PC1, y = PC2, color = target)) + 
+  geom_point() +
+  theme_bw()
+
+loading_scores <- pca$rotation
+
+df_scale <- df_continuous %>% transmute(thalach = (thalach - mean(thalach)) / sd(thalach),
+                                        trestbps = (trestbps - mean(trestbps)) / sd(trestbps),
+                                        oldpeak = (oldpeak - mean(oldpeak)) / sd(oldpeak),
+                                        chol = (chol - mean(chol)) / sd(chol),
+                                        age = (age - mean(age)) / sd(age))
+
+print(sum(loading_scores[,1]* df_scale[1,]))
+print(principal_components[1,1])
+
+pred_matrix <- predict(pca, newdata=df_continuous)
+print(pred_matrix[1,1])
+
+fit <- glm(target ~ ., data=df_pca, family =binomial(link = "logit"))
+summary(fit)
