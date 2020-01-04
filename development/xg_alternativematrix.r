fde7c07ead9bf8425c1017d1df4087f73257fc57
Felix Hauer <hauerf98@gmail.com>
2020-01-04 16:54:10 +0100

    added alternative encoding

--- development/xg_alternativematrix.r
+++ development/xg_alternativematrix.r
@@ -2,27 +2,53 @@ source("helpers.r")
 library(xgboost)
 
 
-df <-  get_training_df_clean()
+# variable f is character
+df <- tibble(x = c(1, 2, 3.4, 5, 100),
+             f = c("a", "a", "b", "b", "C"),
+             y = c(-1, -1.1, 40, 44, -12))
 
-dtrain <- df %>%
-  select(-target) %>%
-  mutate_if(is.character, as.factor) %>%
-  mutate_if(is.factor, as.numeric) %>%
-  select_if(is.numeric) %>%
-  as.matrix()
+# convert f to character
+df <- df %>%
+  mutate(f = factor(f, levels = c("a", "b", "C")))
 
-dtrain <- xgb.DMatrix(label = ifelse(df$target == "no_disease", 0, 1),
-                      data = dtrain)
-
-param <- list(max_depth = 2, eta = 0.3, verbose = 0, nthread = 2,
-              objective = "binary:logistic")
-
-bst <- xgb.train(param, dtrain, nrounds = 5)
+# create a model matrix (-1 to drop intercept) -> one hot encoding
+df_model_matrix <- model.matrix(y ~ .-1, df)
 
 
-y_probabilities <- predict(bst, dtrain)
+dtrain <- xgb.DMatrix(df_model_matrix, label = df$y)
 
-AUC(y_true = ifelse(df$target == "no_disease", 0, 1),
-    y_pred = y_probabilities)
+param <- list(max_depth = 2, eta = 0.3, verbose = 0, nthread = 2)
+
+bst <- xgb.train(param, dtrain, nrounds = 50)
+
+y_pred <- predict(bst, dtrain)
+
+
+
+
+# #false endocing from kaggle
+# 
+# df <-  get_training_df_clean()
+# 
+# dtrain <- df %>%
+#   select(-target) %>%
+#   mutate_if(is.character, as.factor) %>%
+#   mutate_if(is.factor, as.numeric) %>%
+#   select_if(is.numeric) %>%
+#   as.matrix()
+# 
+# dtrain <- xgb.DMatrix(label = ifelse(df$target == "no_disease", 0, 1),
+#                       data = dtrain)
+# 
+# param <- list(max_depth = 2, eta = 0.3, verbose = 0, nthread = 2,
+#               objective = "binary:logistic")
+# 
+# bst <- xgb.train(param, dtrain, nrounds = 5)
+# 
+# 
+# y_probabilities <- predict(bst, dtrain)
+# 
+# AUC(y_true = ifelse(df$target == "no_disease", 0, 1),
+#     y_pred = y_probabilities)
 
 
